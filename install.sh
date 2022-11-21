95303a6c510cca1b0262480b8b285d7f741a1fa9
danteking <danteking@aliyun.com>
2022-11-21 23:01:43 +0800

    feat: add boost libb

--- install.sh
+++ install.sh
@@ -6,10 +6,18 @@ wget https://github.com/yanyiwu/cppjieba/archive/refs/heads/master.zip
 unzip master.zip 
 mv cppjieba-master cppjieba
 rm -rf master.zip
-cd ..
+# download cpp-httplib
+wget https://github.com/yhirose/cpp-httplib/archive/refs/heads/master.zip
+unzip master.zip
+mv cpp-httplib-master cpp-httplib
+rm -rf master.zip
 # download jsoncpp
 sudo yum install jsoncpp-devel
-
-# cd controller
-# make
-# ./hurrydocgo
+# download boost lib
+sudo yum install boost -y
+sudo yum install boost-devel -y
+sudo yum install boost-doc -y
+cd ..
+cd controller
+make
+./hurrydocgo
