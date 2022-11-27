1370998b2860ca5c14f2ad7b9a7b0f23630b2897
danteking <danteking@aliyun.com>
2022-11-27 22:14:24 +0800

    add comments

--- preprocessor/preprocessor.cpp
+++ preprocessor/preprocessor.cpp
@@ -1,9 +1,15 @@
 #include "preprocessor.h"
 namespace hurrydocgo
 {
-
-    bool PreProcessor::EnumFile(const std::string &g_input_path, std::vector<std::string> *file_list)
-    {
+    /**
+     * @brief Enumerate html file names recursively
+     * 
+     * @param g_input_path 
+     * @param file_list 
+     * @return true 
+     * @return false 
+     */
+    bool PreProcessor::EnumFile(const std::string &g_input_path, std::vector<std::string> *file_list) {
       std::cout << g_input_path << std::endl;
       namespace fs = std::filesystem;
       // 输入文件路径类
@@ -34,8 +40,15 @@ namespace hurrydocgo
       return true;
     }
 
-    bool PreProcessor::ParseTitle(const std::string &html, std::string *title)
-    {
+    /**
+     * @brief Get title from a html file
+     * 
+     * @param html 
+     * @param title 
+     * @return true 
+     * @return false 
+     */
+    bool PreProcessor::ParseTitle(const std::string &html, std::string *title) {
 
       size_t begin = html.find(TITLE_BEGIN_LABEL);
       if (begin == std::string::npos)
@@ -65,17 +78,31 @@ namespace hurrydocgo
 
     // local path: ../data/input/html/xxx.html
     // online path: https://www.boost.org/doc/libs/1_80_0/doc/html/xxx.html
-    bool PreProcessor::ParseUrl(const std::string &file_path, std::string *url)
-    {
+    /**
+     * @brief Get html url and concatenate after prefix url
+     * 
+     * @param file_path 
+     * @param url 
+     * @return true 
+     * @return false 
+     */
+    bool PreProcessor::ParseUrl(const std::string &file_path, std::string *url) {
       std::string url_head = "https://www.boost.org/doc/libs/1_80_0/doc";
       std::string url_tail = file_path.substr(g_input_path.size());
       *url = url_head + url_tail;
       return true;
     }
 
-    // remove html label in html string
-    bool PreProcessor::ParseContent(const std::string &html, std::string *content)
-    {
+    // 
+    /**
+     * @brief Remove html label in html string
+     * 
+     * @param html 
+     * @param content 
+     * @return true 
+     * @return false 
+     */
+    bool PreProcessor::ParseContent(const std::string &html, std::string *content) {
       bool is_content = true;
       for (auto e : html)
       {
@@ -107,9 +134,15 @@ namespace hurrydocgo
       return true;
     }
 
-    bool PreProcessor::ParseFile(const std::string &file_path, DocInfo *doc_info)
-    {
-
+    /**
+     * @brief Get title, url and content of a html file
+     * 
+     * @param file_path 
+     * @param doc_info 
+     * @return true 
+     * @return false 
+     */
+    bool PreProcessor::ParseFile(const std::string &file_path, DocInfo *doc_info) {
       // 1.read file content
       std::string html;
       bool ret = Util::ReadFile(file_path, &html);
@@ -143,8 +176,13 @@ namespace hurrydocgo
       return true;
     }
 
-    void PreProcessor::WriteOutput(const DocInfo &doc_info, std::ofstream &ofstream)
-    {
+    /**
+     * @brief Write a doc_info struct with line text form into new file
+     * 
+     * @param doc_info 
+     * @param ofstream 
+     */
+    void PreProcessor::WriteOutput(const DocInfo &doc_info, std::ofstream &ofstream) {
       ofstream << doc_info.title << '\3' << doc_info.url << '\3' << doc_info.content << std::endl;
     }
 
--- preprocessor/preprocessor.h
+++ preprocessor/preprocessor.h
@@ -12,10 +12,6 @@
 // - get title. url and contents wuthout html labels
 // - output is texts
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
 #include <filesystem>
 #include "../common/util.hpp"
 
@@ -26,18 +22,71 @@
 
 namespace hurrydocgo
 {
+
   /**
+   * @brief Preprocessor module
    * 
-  */
+   */
   class PreProcessor
   {
   public:
 
+    /**
+     * @brief Enumerate html file names recursively
+     * 
+     * @param g_input_path 
+     * @param file_list 
+     * @return true 
+     * @return false 
+     */
     static bool EnumFile(const std::string &g_input_path, std::vector<std::string> *file_list); 
+
+    /**
+     * @brief Get title from a html file
+     * 
+     * @param html 
+     * @param title 
+     * @return true 
+     * @return false 
+     */
     static bool ParseTitle(const std::string &html, std::string *title);
+
+    /**
+     * @brief Get html url and concatenate after prefix url
+     * 
+     * @param file_path 
+     * @param url 
+     * @return true 
+     * @return false 
+     */
     static bool ParseUrl(const std::string &file_path, std::string *url);
+
+    /**
+     * @brief Remove html label in html string
+     * 
+     * @param html 
+     * @param content 
+     * @return true 
+     * @return false 
+     */
     static bool ParseContent(const std::string &html, std::string *content);
+
+    /**
+     * @brief Get title, url and content of a html file
+     * 
+     * @param file_path 
+     * @param doc_info 
+     * @return true 
+     * @return false 
+     */
     static bool ParseFile(const std::string &file_path, DocInfo *doc_info);
+
+    /**
+     * @brief Write a doc_info struct with line text form into new file
+     * 
+     * @param doc_info 
+     * @param ofstream 
+     */
     static void WriteOutput(const DocInfo &doc_info, std::ofstream &ofstream);
 
   };
